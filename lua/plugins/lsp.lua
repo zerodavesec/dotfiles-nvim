3cae811db801c6fa099be7c05aad9e81e0b5ed8a
ZeroDave <martinsec20@proton.me>
2025-06-12 22:52:40 +0100

    Added LICENSE.md, created README.md.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -206,8 +206,9 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      ruff = {},
-      pyright = {
+      -- PYTHON Config
+      ruff = {}, -- Linting and Formatting
+      pyright = { -- LSP and Linting
         on_attach = function(client, bufnr)
           -- Disable Pyright's formatting
           client.server_capabilities.documentFormattingProvider = false
@@ -229,31 +230,20 @@ return {
       },
       -- clangd = {},
       -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
-      --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       -- ts_ls = {},
-      --
 
+      -- LUA config
       lua_ls = {
-        -- cmd = { ... },
-        -- filetypes = { ... },
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = 'Replace',
             },
             diagnostics = { disable = { 'missing-fields' } },
-            capabilities = {
-              documentFormattingProvider = false, -- disable formatting from lua_ls
-              documentRangeFormattingProvider = false,
-            },
           },
         },
       },
